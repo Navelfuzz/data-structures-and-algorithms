76646c65b18842bedbfe4d76b9a087b94272aea0
Jonathon Stillson <jcstillson77@gmail.com>
2023-10-06 18:48:27 -0400

    finished cc36

--- java/datastructures/README.md
+++ java/datastructures/README.md
@@ -393,3 +393,20 @@ Features:
   * Arguments: none
   * Returns the total number of vertices in the graph
   * 0 if there are none
+
+___
+
+# Code Challenge: Class 36: Graph Breadth First
+Feature Tasks
+Write the following method for the Graph class:
+
+breadth first
+Arguments: Node
+Return: A collection of nodes in the order they were visited.
+Display the collection
+Structure and Testing
+Utilize the Single-responsibility principle: any methods you write should be clean, reusable, abstract component parts to the whole challenge. You will be given feedback and marked down if you attempt to define a large, complex algorithm in one function definition.
+
+Write at least three test assertions for each method that you define.
+
+Ensure your tests are passing before you submit your solution.
---
+++ java/datastructures/lib/src/test/java/datastructures/graphBF/GraphBFTest.java
@@ -0,0 +1,101 @@
+package datastructures.graphBF;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class GraphBFTest {
+    private Graph<String> graph;
+
+    @BeforeEach
+    public void setUp() {
+        graph = new Graph<>(10);
+    }
+
+    @Test
+    public void addVertexGraphTest() {
+        Vertex<String> vertexA = graph.addVertex("A");
+        assertNotNull(vertexA);
+        assertEquals(1, graph.size());
+    }
+
+    @Test
+    public void addEdgeTest() {
+        Vertex<String> vertexA = graph.addVertex("A");
+        Vertex<String> vertexB = graph.addVertex("B");
+        graph.addEdge(vertexA, vertexB, 3);
+
+        LinkedList<Edge<String>> neighbors = graph.getNeighbors(vertexA);
+        assertEquals(1, neighbors.size());
+        assertEquals("B", neighbors.getFirst().destination.value);
+        assertEquals(3, neighbors.getFirst().weight);
+    }
+
+    @Test
+    void allVerticesTest() {
+        Vertex<String> vertexA = graph.addVertex("A");
+        Vertex<String> vertexB = graph.addVertex("B");
+        Vertex<String> vertexC = graph.addVertex("C");
+
+        LinkedList<Vertex<String>> vertices = graph.getVertices();
+        assertEquals(3, vertices.size());
+        assertTrue(vertices.contains(vertexA));
+        assertTrue(vertices.contains(vertexB));
+        assertTrue(vertices.contains(vertexC));
+    }
+
+    @Test
+    void allNeighborsTest() {
+        Vertex<String> vertexA = graph.addVertex("A");
+        Vertex<String> vertexB = graph.addVertex("B");
+        graph.addEdge(vertexA, vertexB, 2);
+
+        LinkedList<Edge<String>> neighbors = graph.getNeighbors(vertexA);
+        assertEquals(1, neighbors.size());
+        assertEquals("B", neighbors.getFirst().destination.value);
+        assertEquals(2, neighbors.getFirst().weight);
+    }
+
+    @Test
+    void sizeTest() {
+        Vertex<String> vertexA = graph.addVertex("A");
+        Vertex<String> vertexB = graph.addVertex("B");
+
+        assertEquals(2, graph.size());
+    }
+
+    @Test
+    void nodeAndEdgeTest() {
+        Vertex<String> vertexA = graph.addVertex("A");
+        graph.addEdge(vertexA, vertexA, 1);
+
+        assertEquals(1, graph.size());
+        assertEquals(1, graph.getNeighbors(vertexA).size());
+    }
+
+
+    @Test
+    public void breadthFirstTraversalTest() {
+        Vertex<String> vertexA = graph.addVertex("A");
+        Vertex<String> vertexB = graph.addVertex("B");
+        Vertex<String> vertexC = graph.addVertex("C");
+        Vertex<String> vertexD = graph.addVertex("D");
+
+        graph.addEdge(vertexA, vertexB);
+        graph.addEdge(vertexA, vertexC);
+        graph.addEdge(vertexB, vertexD);
+
+        List<Vertex<String>> traversalResult = graph.breadthFirstTraversal(vertexA);
+
+        assertEquals(4, traversalResult.size());
+
+        assertTrue(traversalResult.contains(vertexA));
+        assertTrue(traversalResult.contains(vertexB));
+        assertTrue(traversalResult.contains(vertexC));
+        assertTrue(traversalResult.contains(vertexD));
+    }
+}
