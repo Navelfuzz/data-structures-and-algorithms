054654c8bda6acca491949d4695ca8a09c4e97e9
Jonathon Stillson <jcstillson77@gmail.com>
2023-10-06 19:17:40 -0400

    fixed 36 & 38 mixup

---
+++ java/datastructures/lib/src/main/java/datastructures/graphDF/README.md
@@ -0,0 +1,38 @@
+# Code Challenge 36 - Breadth First Graph
+Implement a depth-first traversal on a graph.
+
+## Whiteboard Process
+![]()
+
+## Approach & Efficiency
+The recursive method visits all the connected nodes from a starting point, and mark/track them as visited using a hashset, 
+and then adding them to a list in the order they were visited.
+The BigO time complexity is O(V+E) and space O(v)
+
+## Solution
+```java
+public void depthFirstHelper(Vertex<T> curr, Set<Vertex<T>> visitedSet, List<Vertex<T>> visitedList) {
+    if (curr == null || visitedSet.contains(curr)) {
+      return;
+    }
+
+    visitedSet.add(curr);
+    visitedList.add(curr);
+
+    for (Edge<T> edge : adjacencyLists.get(curr)) {
+      Vertex<T> neighbor = edge.destination;
+      depthFirstHelper(neighbor, visitedSet, visitedList);
+    }
+  }
+
+  public List<Vertex<T>> depthFirst(Vertex<T> start) {
+    List<Vertex<T>> output = new ArrayList<>();
+    if (!adjacencyLists.containsKey(start)) {
+      return output;
+    }
+
+    Set<Vertex<T>> visitedSet = new HashSet<>();
+    depthFirstHelper(start, visitedSet, output);
+    return output;
+  }
+```
---
+++ java/datastructures/lib/src/test/java/datastructures/graphDF/GraphDFTest.java
@@ -0,0 +1,123 @@
+package datastructures.graphDF;
+
+import datastructures.graphDF.Edge;
+import datastructures.graphDF.Graph;
+import datastructures.graphDF.Vertex;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GraphDFTest {
+    private Graph<String> graph;
+
+    @BeforeEach
+    public void setUp() {
+        graph = new Graph<>(10);
+    }
+
+    @Test
+    public void addVertexGraphTest() {
+        Vertex<String> vertexA = graph.addVertex("A");
+        assertNotNull(vertexA);
+        assertEquals(1, graph.size());
+    }
+
+    @Test
+    public void addEdgeTest() {
+        Vertex<String> vertexA = graph.addVertex("A");
+        Vertex<String> vertexB = graph.addVertex("B");
+        graph.addEdge(vertexA, vertexB, 3);
+
+        LinkedList<Edge<String>> neighbors = graph.getNeighbors(vertexA);
+        assertEquals(1, neighbors.size());
+        assertEquals("B", neighbors.getFirst().destination.value);
+        assertEquals(3, neighbors.getFirst().weight);
+    }
+
+    @Test
+    void allVerticesTest() {
+        Vertex<String> vertexA = graph.addVertex("A");
+        Vertex<String> vertexB = graph.addVertex("B");
+        Vertex<String> vertexC = graph.addVertex("C");
+
+        LinkedList<Vertex<String>> vertices = graph.getVertices();
+        assertEquals(3, vertices.size());
+        assertTrue(vertices.contains(vertexA));
+        assertTrue(vertices.contains(vertexB));
+        assertTrue(vertices.contains(vertexC));
+    }
+
+    @Test
+    void allNeighborsTest() {
+        Vertex<String> vertexA = graph.addVertex("A");
+        Vertex<String> vertexB = graph.addVertex("B");
+        graph.addEdge(vertexA, vertexB, 2);
+
+        LinkedList<Edge<String>> neighbors = graph.getNeighbors(vertexA);
+        assertEquals(1, neighbors.size());
+        assertEquals("B", neighbors.getFirst().destination.value);
+        assertEquals(2, neighbors.getFirst().weight);
+    }
+
+    @Test
+    void sizeTest() {
+        Vertex<String> vertexA = graph.addVertex("A");
+        Vertex<String> vertexB = graph.addVertex("B");
+
+        assertEquals(2, graph.size());
+    }
+
+    @Test
+    void nodeAndEdgeTest() {
+        Vertex<String> vertexA = graph.addVertex("A");
+        graph.addEdge(vertexA, vertexA, 1);
+
+        assertEquals(1, graph.size());
+        assertEquals(1, graph.getNeighbors(vertexA).size());
+    }
+
+
+    @Test
+    public void breadthFirstTraversalTest() {
+        Vertex<String> vertexA = graph.addVertex("A");
+        Vertex<String> vertexB = graph.addVertex("B");
+        Vertex<String> vertexC = graph.addVertex("C");
+        Vertex<String> vertexD = graph.addVertex("D");
+
+        graph.addEdge(vertexA, vertexB);
+        graph.addEdge(vertexA, vertexC);
+        graph.addEdge(vertexB, vertexD);
+
+        List<Vertex<String>> traversalResult = graph.breadthFirstTraversal(vertexA);
+
+        assertEquals(4, traversalResult.size());
+
+        assertTrue(traversalResult.contains(vertexA));
+        assertTrue(traversalResult.contains(vertexB));
+        assertTrue(traversalResult.contains(vertexC));
+        assertTrue(traversalResult.contains(vertexD));
+    }
+    @Test
+    public void depthFirstTest() {
+        Vertex<String> a = graph.addVertex("A");
+        Vertex<String> b = graph.addVertex("B");
+        Vertex<String> c = graph.addVertex("C");
+        Vertex<String> d = graph.addVertex("D");
+        Vertex<String> e = graph.addVertex("E");
+
+        graph.addEdge(a, b);
+        graph.addEdge(a, c);
+        graph.addEdge(b, d);
+        graph.addEdge(b, e);
+
+        List<Vertex<String>> output = graph.depthFirst(a);
+
+        List<Vertex<String>> expected = List.of(a, b, d, e, c);
+
+        assertEquals(expected, output);
+    }
+}
